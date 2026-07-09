67ac31c489ea3e19a0ce78ac2a5e2c05b8618932
Morse Micro <info@morsemicro.com>
2026-07-09 23:21:05 +1000

    target: mediatek: add Morse Micro EKH19
    This collects the target definitions from mm/v23.05.6 @ 6b04f456c9
    for the ekh19.
    
    For continued history, see:
    git log --oneline 6b04f456c9 -- target/linux/mediatek/

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1006,6 +1006,7 @@ define Device/glinet_gl-mt3000
   DEVICE_DTS := mt7981b-glinet-gl-mt3000
   DEVICE_DTS_DIR := ../dts
   SUPPORTED_DEVICES += glinet,mt3000-snand
+  
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-hwmon-pwmfan kmod-usb3
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
@@ -1015,6 +1016,17 @@ define Device/glinet_gl-mt3000
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-gl-metadata
 endef
 TARGET_DEVICES += glinet_gl-mt3000
+
+define Device/morse_ekh19
+  $(Device/glinet_gl-mt3000)
+  DEVICE_VENDOR := MorseMicro
+  DEVICE_MODEL := MM8108-EKH19
+  SUPPORTED_DEVICES += glinet,gl-mt3000
+  # Image name format : <openwrt>-<morse-x.x.x>-<morsemicro>-<mm8108-ekh19>-squashfs-sysupgrade.bin
+  IMAGE_PREFIX = $$(VERSION_DIST_SANITIZED)-$$(IMG_PREFIX_VERCODE)$$(IMG_PREFIX_EXTRA)$$(call sanitize,$$(DEVICE_TITLE))
+  DEVICE_IMG_NAME = $$(IMAGE_PREFIX)-$$(1)-$$(2)
+endef
+TARGET_DEVICES += morse_ekh19
 
 define Device/glinet_gl-mt6000
   DEVICE_VENDOR := GL.iNet
